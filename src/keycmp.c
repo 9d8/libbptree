c49bb732c2c80f58f89b5a0b30ad10d4a9136ac8
9d8 <42924931+9d8@users.noreply.github.com>
2018-11-14 23:23:52 -0500

    Added set_keycmp() function to change the keycmp of a b+tree.

--- src/keycmp.c
+++ src/keycmp.c
@@ -8,3 +8,7 @@ int bptree_keycmp_int(bptree_key key1, bptree_key key2) {
 int bptree_keycmp_str(bptree_key key1, bptree_key key2) {
 	return strcmp(key1.str, key2.str);
 }
+
+void bptree_set_keycmp(bptree* bpt, int (*key_compare)(bptree_key key1, bptree_key key2)) {
+	bpt->key_compare = key_compare;	
+}
--- src/keycmp.h
+++ src/keycmp.h
@@ -5,5 +5,6 @@
 
 int bptree_keycmp_int(bptree_key key1, bptree_key key2);
 int bptree_keycmp_str(bptree_key key1, bptree_key key2);
+void bptree_set_keycmp(bptree* bpt, int (*key_compare)(bptree_key key1, bptree_key key2));
 
 #endif
